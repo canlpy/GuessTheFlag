5436322fb8495823930f7e19b99845357f66cdf3
CNLPY <calpay@yahoo.com>
2022-06-03 13:29:43 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Can on 3.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
